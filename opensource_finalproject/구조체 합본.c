382b030d327e5c9a4c10a9587bb0c5a0602d9222
kimmi <kimminjea31@naver.com>
2024-06-18 00:51:56 +0900

    초기 커밋.

---
+++ opensource_finalproject/구조체 합본.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ResearchPaper_contents 구조체 정의
+typedef struct {
+    int page;
+    char* contents;
+} ResearchPaper_contents;
+
+// ResearchPaper 구조체 정의
+typedef struct ResearchPaper {
+    char* title;
+    char* author;
+    int year;
+    char* url;
+    struct ContentNode* contentsHead; // 논문 내용의 연결리스트의 시작 노드
+    struct ResearchPaper* next;
+} ResearchPaper;
+
+// 논문 내용의 노드 구조체 정의
+typedef struct ContentNode {
+    ResearchPaper_contents paperContent;
+    struct ContentNode* next;
+} ContentNode;
+
+// 문자열을 복사하는 함수
+char* my_strdup(const char* str) {
+    char* copy = (char*)malloc(strlen(str) + 1);
+    if (copy) {
+        strcpy(copy, str);
+    }
+    return copy;
+}
+
+// 논문 연결리스트의 시작 노드
+ResearchPaper* head = NULL;
+
+// 새로운 논문 노드를 생성하고 연결리스트에 추가하는 함수
+void addPaper(char* title, char* author, int year, char* url) {
+    // 새로운 논문 노드 생성
+    ResearchPaper* newPaper = (ResearchPaper*)malloc(sizeof(ResearchPaper));
+
+    // ResearchPaper 값 설정
+    newPaper->title = my_strdup(title);
+    newPaper->author = my_strdup(author);
+    newPaper->year = year;
+    newPaper->url = my_strdup(url);
+    newPaper->contentsHead = NULL; // 내용의 시작 노드는 NULL로 초기화
+    newPaper->next = NULL;
+
+    // 리스트가 비어있으면 새로운 논문 노드를 시작 노드로 설정
+    if (head == NULL) {
+        head = newPaper;
+    }
+    else {
+        // 리스트의 끝에 새로운 논문 노드 추가
+        ResearchPaper* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newPaper;
+    }
+}
+
+// 논문 내용 노드를 추가하는 함수
+void addPaperContent(ResearchPaper* paper, int page, char* contents) {
+    // 새로운 논문 내용 노드 생성
+    ContentNode* newNode = (ContentNode*)malloc(sizeof(ContentNode));
+
+    // ResearchPaper_contents 값 설정
+    newNode->paperContent.page = page;
+    newNode->paperContent.contents = my_strdup(contents);
+    newNode->next = NULL;
+
+    // 내용 리스트가 비어있으면 새로운 내용을 시작 노드로 설정
+    if (paper->contentsHead == NULL) {
+        paper->contentsHead = newNode;
+    }
+    else {
+        // 내용 리스트의 끝에 새로운 내용 노드 추가
+        ContentNode* temp = paper->contentsHead;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// 연결리스트의 모든 논문과 그 내용을 출력하는 함수
+void printPapers() {
+    ResearchPaper* paperTemp = head;
+    while (paperTemp != NULL) {
+        printf("제목: %s\n", paperTemp->title);
+        printf("저자: %s\n", paperTemp->author);
+        printf("연도: %d\n", paperTemp->year);
+        printf("주소: %s\n", paperTemp->url);
+
+        ContentNode* contentTemp = paperTemp->contentsHead;
+        while (contentTemp != NULL) {
+            printf("  페이지: %d\n", contentTemp->paperContent.page);
+            printf("  내용: %s\n", contentTemp->paperContent.contents);
+            contentTemp = contentTemp->next;
+        }
+        printf("\n");
+        paperTemp = paperTemp->next;
+    }
+}
+
+// 메인 함수
+int main() {
+    char title[100];
+    char author[100];
+    int year;
+    char url[100];
+    int page;
+    char contents[500];
+
+    while (1) {
+        printf("논문 제목 (exit 입력시 종료): ");
+        scanf("%99s", title);
+        if (strcmp(title, "exit") == 0) {
+            break;
+        }
+
+        printf("저자: ");
+        scanf("%99s", author);
+
+        printf("연도: ");
+        scanf("%d", &year);
+
+        printf("URL 주소: ");
+        scanf("%99s", url);
+
+        addPaper(title, author, year, url);
+
+        ResearchPaper* currentPaper = head;
+        while (currentPaper->next != NULL) {
+            currentPaper = currentPaper->next;
+        }
+
+        while (1) {
+            printf("페이지를 입력하시오 (종료: -1): ");
+            scanf("%d", &page);
+            if (page == -1) {
+                break;
+            }
+
+            getchar(); // 버퍼 비우기
+            printf("내용을 입력하시오: ");
+            fgets(contents, sizeof(contents), stdin);
+            contents[strcspn(contents, "\n")] = 0; // fgets로 입력받은 문자열에서 개행문자를 제거
+
+            addPaperContent(currentPaper, page, contents);
+        }
+    }
+
+    printf("\n논문 리스트:\n");
+    printPapers();
+
+    return 0;
+}
---
+++ opensource_finalproject/구조체_논문내용.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ResearchPaper_contents 구조체 정의
+typedef struct {
+    int page;
+    char* contents;
+} ResearchPaper_contents;
+
+// 노드 구조체 정의
+typedef struct Node {
+    ResearchPaper_contents paperContent;
+    struct Node* next;
+} Node;
+
+// 문자열을 복사하는 함수
+char* my_strdup(const char* str) {
+    char* copy = (char*)malloc(strlen(str) + 1);
+    if (copy) {
+        strcpy(copy, str);
+    }
+    return copy;
+}
+
+// 연결리스트의 시작 노드
+Node* head = NULL;
+
+// 새로운 노드를 생성하고 연결리스트에 추가하는 함수
+void addPaperContent(int page, char* contents) {
+    // 새로운 노드 생성
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    // ResearchPaper_contents 값 설정
+    newNode->paperContent.page = page;
+    newNode->paperContent.contents = my_strdup(contents);
+    newNode->next = NULL;
+
+    // 리스트가 비어있으면 새로운 노드를 시작 노드로 설정
+    if (head == NULL) {
+        head = newNode;
+    }
+    else {
+        // 리스트의 끝에 새로운 노드 추가
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// 연결리스트의 모든 노드를 출력하는 함수
+void printPaperContents() {
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("페이지 : %d\n", temp->paperContent.page);
+        printf("중요 내용 : %s\n\n", temp->paperContent.contents);
+        temp = temp->next;
+    }
+}
+
+// 메인 함수
+int main()
+{
+    int page;
+    char contents[500]; // 내용을 저장할 배열 크기를 충분히 크게 설정
+
+    while (1) {
+        printf("페이지를 입력하시오 (종료 : -1) : ");
+        scanf("%d", &page);
+        if (page == -1) {
+            break;
+        }
+
+        printf("내용을 입력하시오 : ");
+        getchar(); // 버퍼 비우기
+        fgets(contents, sizeof(contents), stdin);
+        contents[strcspn(contents, "\n")] = 0; // fgets로 입력받은 문자열에서 개행문자를 제거
+
+        addPaperContent(page, contents);
+    }
+
+    printf("\n논문 내용 리스트 :\n");
+    printPaperContents();
+
+    return 0;
+}
